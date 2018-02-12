ac445fbdfaa05a8176c99e6c7e3307b7838c8a49
Annabel Consilvio <annabelconsilvio@Annabels-MacBook-Air.local>
2018-02-12 16:13:50 -0500

    removing extra file, updating error response on comment model

--- routes/comment.js
+++ routes/comment.js
@@ -1,19 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-// const objectmapper = require('../db/objectmapper');
 var CommentModel = require('../models/CommentModel');
 
 /* GET home page. */
 router.post('/',  (req, res, next) => {
 	console.log(req.body)
-	// objectmapper.mapCommentToDatabase(req.body);
 	CommentModel.insertToDb(req.body)
 	.then(() => {
-		res.json({message: "this worked?"});
+		res.status(200).json({message: "this worked?"});
 	})
 	.catch(error => {
-		res.json({message: error.message});
+		res.status(500).json({error: error.message});
 	})
 });
 
