8731477535dab20124ae12545eeb89eea9041326
Franton Lin <franton.lin@students.olin.edu>
2018-04-05 21:38:35 -0400

    Changed back to dotenv for environment variables. It's much easier to use with Docker!

--- db/db.js
+++ db/db.js
@@ -1,12 +1,17 @@
 const pgp = require('pg-promise')();
 
-const connection = {
+let connection = {
   host: process.env.DB_HOST, 
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
 };
+if (process.env.NODE_ENV === 'test') {
+  connection.database = process.env.DB_NAME_TEST;
+  connection.user = process.env.DB_USER_TEST;
+  connection.password = process.env.DB_PASS_TEST;
+}
 
 const db = pgp(connection);
 
